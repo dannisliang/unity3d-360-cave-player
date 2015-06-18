d5c36fd5fbf0af5840a1197cef9ea0bc7b4dfed3
Gabriel Roque <gabriel.roque.2005@gmail.com>
2015-06-18 11:51:49 -0300

    Adicionado pasta de Assets

---
+++ Assets/Scripts/ScriptVideo360.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioSource))]
+public class ScriptVideo360 : MonoBehaviour
+{
+	string url = "http://www.unity3d.com/webplayers/Movie/sample.ogg";
+	MovieTexture sphereTexture;
+
+	void Start ()
+	{
+		StartCoroutine (LoadMovie (@url));
+	}
+
+	IEnumerator LoadMovie (string url)
+	{
+		Debug.Log ("Load Movie");
+		WWW videoStream = new WWW (url);
+		sphereTexture = videoStream.movie;
+		GetComponent <AudioSource>().clip = videoStream.audioClip;
+
+		float startLoadTime = Time.timeSinceLevelLoad;
+
+		while (!sphereTexture.isReadyToPlay)
+		{
+			yield return 0;
+		}
+		
+		Debug.Log ("Time to be ready: " + (Time.timeSinceLevelLoad - startLoadTime).ToString () );
+		Debug.Log ("Ready!");
+		GetComponent<Renderer> ().material.mainTexture = sphereTexture;
+		GetComponent<AudioSource> ().Play ();
+		sphereTexture.Play ();
+	}
+}
