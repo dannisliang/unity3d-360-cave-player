0e2ca3951fdf23a785fb24ddf569b916a8258520
Caverna Digital <cavernadigital@gmail.com>
2015-06-18 18:10:55 -0300

    Versão funcional na caverna  - abrindo apenas um arquivo de video  - arquivo precisa estar no D:\Videos360\video360.ogg

--- Assets/Scripts/ScriptVideo360.cs
+++ Assets/Scripts/ScriptVideo360.cs
@@ -1,20 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 [RequireComponent (typeof (AudioSource))]
 public class ScriptVideo360 : MonoBehaviour
 {
-	string url = "http://www.unity3d.com/webplayers/Movie/sample.ogg";
+	public Camera cam;
+	public bool debugEnabled;
+	public Text debugText;
+	public GameObject canvas;
+
+//	string url = "http://www.unity3d.com/webplayers/Movie/sample.ogg";
+//	string url = "http://10.0.200.241:8080/panoramic.ogg";
+	string url = "file:///D:/Videos360/video360.ogg";
 	MovieTexture sphereTexture;
+
+	void Awake ()
+	{
+		cam.aspect = 1f;
+		cam.fieldOfView = 90f;
+
+		canvas.SetActive (debugEnabled);
+	}
 
 	void Start ()
 	{
 		StartCoroutine (LoadMovie (@url));
+//		LoadFromResource ();
 	}
 
 	IEnumerator LoadMovie (string url)
 	{
-		Debug.Log ("Load Movie");
+		debugText.text += "\nLoading...";
+
 		WWW videoStream = new WWW (url);
 		sphereTexture = videoStream.movie;
 		GetComponent <AudioSource>().clip = videoStream.audioClip;
@@ -23,13 +41,26 @@ public class ScriptVideo360 : MonoBehaviour
 
 		while (!sphereTexture.isReadyToPlay)
 		{
+			if (!string.IsNullOrEmpty(videoStream.error))
+			{
+				debugText.text += videoStream.error;
+				return false;
+			}
 			yield return 0;
 		}
-		
-		Debug.Log ("Time to be ready: " + (Time.timeSinceLevelLoad - startLoadTime).ToString () );
-		Debug.Log ("Ready!");
+		debugText.text += "\nTime " + (Time.timeSinceLevelLoad - startLoadTime).ToString ();
+		debugText.text += "\nReady!";
+
 		GetComponent<Renderer> ().material.mainTexture = sphereTexture;
 		GetComponent<AudioSource> ().Play ();
 		sphereTexture.Play ();
+		sphereTexture.loop = true;
 	}
+
+//	void LoadFromResource ()
+//	{
+//		MovieTexture texture = (MovieTexture)Resources.Load ("sample", typeof(MovieTexture));
+//		GetComponent <Renderer>().material.mainTexture =  texture;
+//		texture.Play ();
+//	}
 }
